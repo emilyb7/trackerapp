0a0708d5edf8c3b818f3e4fb38e4b032277f561e
emily <limeyb7@gmail.com>
2018-04-15 19:56:41 +0100

    get books 📚 📖 ✨

--- lib/books/books.ex
+++ lib/books/books.ex
@@ -1,6 +1,8 @@
 defmodule Tracker.Book do
   use Ecto.Schema
+
   import Ecto.Changeset
+
   alias Tracker.Book
 
   # This is the one that includes cast
@@ -11,15 +13,19 @@ defmodule Tracker.Book do
     field :cover, :string
     timestamps()
   end
-
   @required_fields ~w(title isbn author cover)
   @optional_fields ~w()
-
   def changeset(book, params \\ :empty) do
     book
     |> cast(params, @required_fields, @optional_fields)
     |> unique_constraint(:isbn)
   end
+
+
+  def get_books(limit \\ 20) do
+    Tracker.Repo.all Book, limit: limit
+  end
+
 
   def create(book_params) do
     changeset = changeset(%Book{}, book_params)
--- test/books/books_test.exs
+++ test/books/books_test.exs
@@ -3,23 +3,43 @@ defmodule Tracker.BooksTest do
 
   use ExUnit.Case
 
-  test "adding a book" do
+  setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+  end
+  test "create adds a new book to DB" do
     previousCount =
       Book
       |> Repo.all()
       |> Enum.count()
     assert Book.create(%{
-                                   title: "Charlottes Web",
-                                   isbn: "9780064400558",
-                                   author: "E.B. White",
-                                   cover: "https://covers.openlibrary.org/w/id/8156475-M.jpg",
-                                 }) ==
+                         title: "Charlottes Web",
+                         isbn: "9780064400558",
+                         author: "E.B. White",
+                         cover: "https://covers.openlibrary.org/w/id/8156475-M.jpg",
+                       }) ==
              :ok
     newCount =
       Book
       |> Repo.all()
       |> Enum.count()
     assert newCount == previousCount + 1
+  end
+  test "get_books returns empty list when no books available" do
+    # no books yet
+    assert Book.get_books(0) == []
+    assert Book.get_books(1) == []
+  end
+  test "get_books returns list of available books" do
+    test_book = %{
+      title: "Charlottes Web",
+      isbn: "9780064400558",
+      author: "E.B. White",
+      cover: "https://covers.openlibrary.org/w/id/8156475-M.jpg",
+    }
+    # create a book
+    Book.create(test_book) == :ok
+    assert Book.get_books(0) == []
+    assert Book.get_books(10)
+             |> Enum.count() == 1
   end
 end
