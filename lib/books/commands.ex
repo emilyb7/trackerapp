a1c8863e9b8ad4d77bdf5efa026988287794d246
emily <limeyb7@gmail.com>
2018-04-15 11:26:33 +0100

    update create function to accept book data params 📚i

--- lib/books/commands.ex
+++ lib/books/commands.ex
@@ -1,15 +1,21 @@
 defmodule Tracker.Books_Commands do
   alias Tracker.{Books, Repo}
 
-  def create() do
+  def create(book_params) do
     changeset =
       Books.changeset(%Books{},
-                      %{title: "abc", isbn: "1234", author: "JB", cover: ""})
-
+                      %{
+                        title: book_params["title"],
+                        isbn: book_params["isbn"],
+                        author: book_params["author"],
+                        cover: book_params["cover"],
+                      })
     case Repo.insert(changeset) do
-     {:ok, _record} -> "ok"
-     {:error, _changeset} -> "error"
+      {:ok, _record} ->
+        "ok"
+
+      {:error, _changeset} ->
+        "error"
     end
   end
-
 end
--- test/books/books_test.exs
+++ test/books/books_test.exs
@@ -1,12 +1,24 @@
 defmodule Tracker.BooksTest do
-  alias Tracker.{Books,Books_Commands,Repo}
+  alias Tracker.{Books, Books_Commands, Repo}
+
   use ExUnit.Case
 
   test "adding a book" do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
-    previousCount = Books |> Repo.all |> Enum.count
-    Books_Commands.create()
-    newCount = Books |> Repo.all |> Enum.count
+    previousCount =
+      Books
+      |> Repo.all()
+      |> Enum.count()
+    Books_Commands.create %{
+                            title: "Charlottes Web",
+                            isbn: "9780064400558",
+                            author: "E.B. White",
+                            cover: "https://covers.openlibrary.org/w/id/8156475-M.jpg",
+                          }
+    newCount =
+      Books
+      |> Repo.all()
+      |> Enum.count()
     assert newCount == previousCount + 1
   end
 end
