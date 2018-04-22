95fda4c31c73a30d98d1fd9ec89885b52aeb1493
emily <limeyb7@gmail.com>
2018-04-22 15:17:57 +0100

    get_env

--- lib/web/server.ex
+++ lib/web/server.ex
@@ -3,7 +3,16 @@ defmodule Server do
   require Logger
 
   def start(_type, _args) do
+<<<<<<< HEAD
     port = Application.get_env(:tracker, :cowboy_port, 8080)
+=======
+    Logger.info("STARTING!")
+    import Supervisor.Spec
+
+    port = System.get_env("PORT")
+
+    Logger.info(port)
+>>>>>>> c370474... get_env
     children = [Plug.Adapters.Cowboy.child_spec(:http, Router, [], port: port), Tracker.Repo]
 
     Logger.info("started application")
