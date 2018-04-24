8e30b71ba77aab39ca445a6f84c50990631237f2
emily <limeyb7@gmail.com>
2018-04-24 22:26:27 +0100

    fix linter issues 👕 👕  👕  👕

--- client/src/BarcodeReader.jsx
+++ client/src/BarcodeReader.jsx
@@ -1,59 +1,59 @@
-import React from "react";
-import Quagga from "quagga";
+import React from 'react'
+import Quagga from 'quagga'
 
 class BarcodeReader extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { result: null };
+    super(props)
+    this.state = { result: null, }
   }
   componentDidMount = () => {
-    this.init();
+    this.init()
   };
-  componentDidUpdate = () => console.log(this.state);
+  componentDidUpdate = () => console.log(this.state) //eslint-disable-line no-console
 
   init = () => {
     Quagga.init(
       {
         inputStream: {
-          name: "Live",
-          type: "LiveStream",
-          target: document.querySelector("#target") // Or '#yourElement' (optional)
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#target'),
         },
         decoder: {
-          readers: ["code_128_reader", "ean_reader"]
-        }
+          readers: [ 'code_128_reader', 'ean_reader', ],
+        },
       },
       function(err) {
         if (err) {
-          console.log(err);
-          return;
+          console.log(err) //eslint-disable-line no-console
+          return
         }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
+        console.log('Initialization finished. Ready to start') //eslint-disable-line no-console
+        Quagga.start()
       }
-    );
-    Quagga.onDetected(this.onDetected);
+    )
+    Quagga.onDetected(this.onDetected)
   };
 
   onDetected = data => {
-    console.log(data);
-    this.setState({ result: data.codeResult.code });
+    console.log(data) //eslint-disable-line no-console
+    this.setState({ result: data.codeResult.code, })
   };
 
   stop = () => {
-    Quagga.stop();
+    Quagga.stop()
   };
 
   render = () =>
-    <div style={{ position: "relative" }}>
+    <div style={{ position: 'relative', }}>
       <span id="target">Read me</span>
       <span
         onClick={this.stop}
-        style={{ position: "absolute", top: 0, right: 0 }}
+        style={{ position: 'absolute', top: 0, right: 0, }}
       >
         stop
       </span>
     </div>;
 }
 
-export default BarcodeReader;
+export default BarcodeReader
