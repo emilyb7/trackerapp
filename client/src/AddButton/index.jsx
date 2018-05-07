69fe179fc0171200f0f7d65cbb6c64e77ba8f973
emily <limeyb7@gmail.com>
2018-05-07 21:58:30 +0100

    use font awesome ✨ 💄 💜

--- client/src/AddButton/index.jsx
+++ client/src/AddButton/index.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { Link, } from 'react-router-dom'
 
 const AddButton = () => (
-  <Link className="fixed" to="/scan" style={{ bottom: '6px', right: '6px', }}>
-    <div className="br-100 w3 h3 pointer bg-gray" />
+  <Link
+    className="fixed"
+    to="/scan"
+    style={{ bottom: '6px', right: '6px', }}
+    title="Add new book"
+  >
+    <div className="br-100 flex justify-center items-center w3 h3 pointer bg-gray">
+      <i className="fas fa-plus f2 white" />
+    </div>
   </Link>
 )
 
