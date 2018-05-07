3351512b76095e858a2a5ddd3b23afc73235c4bc
emily <limeyb7@gmail.com>
2018-05-07 22:48:10 +0100

    button styling 💅

--- client/src/AddButton/index.jsx
+++ client/src/AddButton/index.jsx
@@ -8,7 +8,7 @@ const AddButton = () => (
     style={{ bottom: '6px', right: '6px', }}
     title="Add new book"
   >
-    <div className="br-100 flex justify-center items-center w3 h3 pointer bg-gray">
+    <div className="br-100 flex justify-center items-center h3 pointer bg-gray shadow-5 w3">
       <i className="fas fa-plus f2 white" />
     </div>
   </Link>
