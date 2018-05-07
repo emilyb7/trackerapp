f8767f82c9c741a0086f098fd2730c63e0207a0a
emily <limeyb7@gmail.com>
2018-05-07 20:58:07 +0100

    book tile styling + placeholder image for missing covers 📸 📕 💅

--- client/src/Library/Tile.jsx
+++ client/src/Library/Tile.jsx
@@ -2,11 +2,46 @@ import React from 'react'
 
 const Tile = ({ author, cover, title, }) => {
   return (
-    <li className="list pv2">
-      <div className="pa2 pb3">
-        <img src={cover} alt="" />
-        <h3 className="ma2">{title}</h3>
-        <span className="ma2">{author}</span>
+    <li className="list pv3 ml3">
+      <div
+        style={{ height: '255px', width: '180px', }}
+        className="bg-light-silver"
+      >
+        {cover ? (
+          <img src={cover} alt={title} />
+        ) : (
+          <svg
+            height={255}
+            width={180}
+            fill="red"
+            viewBox="0 0 180 255"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect fill="silver" x="0" y="0" width="180" height="255" />
+            <text
+              className="code tracked"
+              x="90"
+              y="100"
+              fontFamily="monospace"
+              fontSize="25"
+              fill="gray"
+              textAnchor="middle"
+            >
+              {title}
+            </text>
+            <text
+              className="code tracked"
+              x="90"
+              y="160"
+              fontFamily="monospace"
+              fontSize="18"
+              fill="gray"
+              textAnchor="middle"
+            >
+              {author}
+            </text>
+          </svg>
+        )}
       </div>
     </li>
   )
--- client/src/Library/index.jsx
+++ client/src/Library/index.jsx
@@ -4,7 +4,7 @@ import Tile from './Tile'
 const Library = ({ books, }) => {
   return (
     <div className="bg-near-white  gray  overflow-scroll vh-90">
-      <ul className="flex flex-wrap justify-start h-100">
+      <ul className="content-start flex flex-wrap justify-start h-100">
         {books && books.map(book => <Tile key={book.id} {...book} />)}
       </ul>
     </div>
