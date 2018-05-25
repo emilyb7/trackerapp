d8e8d392b0f03869f593340721595f2459b16f72
emily <limeyb7@gmail.com>
2018-05-25 23:09:21 +0100

    tidying up library function ⚡️ 🗃 📚

--- server/lib/library/library.ex
+++ server/lib/library/library.ex
@@ -6,28 +6,25 @@ defmodule Tracker.Library do
   def fetch(isbn) do
     library_url(isbn)
     |> HTTPoison.get(@headers, @user_agent)
-    |> handle_response
+    |> handle_response(isbn)
   end
 
   defp library_url(isbn) do
     "http://openlibrary.org/api/books\?bibkeys\=ISBN:#{isbn}\&format\=json\&jscmd\=data"
   end
 
-  defp handle_response({:ok, %{status_code: 200, body: "{}"}}) do
+  # no matching book
+  defp handle_response({:ok, %{status_code: 200, body: "{}"}}, _isbn) do
     {:nothing, "nowt here"}
   end
 
-  defp handle_response({:ok, %{status_code: 200, body: body}}) do
-    body = fetch_request_body_data(body)
-    title = Map.fetch!(body, "title")
-    author = Map.fetch!(body, "authors") |> Enum.at(0) |> Map.fetch!("name")
-    cover = get_cover(body)
-    isbn = Map.fetch!(body, "identifiers") |> Map.fetch!("isbn_13") |> Enum.at(0)
+  defp handle_response({:ok, %{status_code: 200, body: body}}, isbn) do
+    book_data = fetch_request_body_data(body)
 
     response = %{
-      :title => title,
-      :author => author,
-      :cover => cover,
+      :title => get_title(book_data),
+      :author => get_author(book_data),
+      :cover => get_cover(book_data),
       :isbn => isbn
     }
 
@@ -35,7 +32,7 @@ defmodule Tracker.Library do
   end
 
   # error case
-  defp handle_response({_, %{status_code: _, body: body}}) do
+  defp handle_response({_, %{status_code: _, body: body}}, _isbn) do
     {:error, Poison.Parser.parse!(body)}
   end
 
@@ -44,10 +41,14 @@ defmodule Tracker.Library do
   end
 
   defp get_cover(book_data) do
-    case Map.fetch(book_data, "cover") do
-      {:ok, cover} ->
-         Map.fetch!(cover, "medium")
-      :error -> nil
-    end
+    get_in(book_data, ["cover", "medium"])
+  end
+
+  defp get_title(book_data) do
+    Map.fetch!(book_data, "title")
+  end
+
+  defp get_author(book_data) do
+    book_data |> Map.fetch!("authors") |> Enum.at(0) |> Map.fetch!("name")
   end
 end
--- server/test/library/library_test.exs
+++ server/test/library/library_test.exs
@@ -4,7 +4,12 @@ defmodule Tracker.LibraryTest do
   import Mock
 
   defp succesful_response do
-    test_data = Path.join(["test", "test-data", "open-library.json"]) |> File.read!
+    test_data = read_example_data("charlottes-web")
+    {:ok, %{status_code: 200, body: test_data}}
+  end
+
+  defp no_image_response do
+    test_data = read_example_data("emma")
     {:ok, %{status_code: 200, body: test_data}}
   end
 
@@ -16,6 +21,10 @@ defmodule Tracker.LibraryTest do
     {:ok, %{status_code: 404, body: "{\"error\": \"error\"}"}}
   end
 
+  defp read_example_data(book_title) do
+    Path.join(["test", "test-data", book_title]) <> ".json" |> File.read!
+  end
+
   def mock_request(response) do
     with_mock HTTPoison, [get: fn(_a, _b, _url) -> response end] do
       Library.fetch("whatever")
@@ -25,11 +34,26 @@ defmodule Tracker.LibraryTest do
   test "library fetch success case" do
     {:ok, %{
       :title => title,
-      :author => _,
-      :cover => _,
+      :author => author,
+      :cover => cover,
       :isbn => _}
     } = succesful_response() |> mock_request
+    assert title === "Charlotte's Web"
+    assert cover === "https://covers.openlibrary.org/b/id/8140481-M.jpg"
+    assert author === "E. B. White"
+
+
+  end
+
+  test "library fetch success case when no image available" do
+    {:ok, %{
+      :title => title,
+      :author => _,
+      :cover => cover,
+      :isbn => _}
+    } = no_image_response() |> mock_request
     assert title === "Emma"
+    assert cover === nil
   end
 
   test "library fetch no results" do
