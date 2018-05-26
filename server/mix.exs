dc5ccf4e9e2dfdb4f6123947b42a161175b0ed22
emily <limeyb7@gmail.com>
2018-05-26 22:42:06 +0100

    upgrade elixir version 👩🏼‍🔬  ⬆️

--- server/mix.exs
+++ server/mix.exs
@@ -5,23 +5,21 @@ defmodule Tracker.Mixfile do
     [
       app: :tracker,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      deps: deps()
     ]
   end
-
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :httpoison],
       mod: {Server, []},
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod
     ]
   end
-
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
@@ -32,10 +30,11 @@ defmodule Tracker.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:cowboy, "~> 1.0.0"},
       {:plug, "~> 1.0"},
-      {:cors_plug ,"~> 1.5"},
+      {:cors_plug, "~> 1.5"},
       {:mock, "~> 0.3.0", [only: :test]}
     ]
   end
 end
+
 # {:dep_from_hexpm, "~> 0.3.0"},
 # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
