064899a00f92dd6b4095bb8f01a0655f8cb9fdfe
emily <limeyb7@gmail.com>
2018-05-31 23:00:48 +0100

    svg improvements 🐶

--- client/src/Library/Placeholder.jsx
+++ client/src/Library/Placeholder.jsx
@@ -3,36 +3,29 @@ import React from 'react'
 const Placeholder = ({ title, author, id, }) => (
   <svg
     id={`placeholder_${id}`}
-    style={{ maxWidth: '48vw', height: '100%', }}
+    className="bg-moon-gray h-100"
+    style={{ maxWidth: '48vw', }}
     height={255}
     width={180}
     fill="silver"
     viewBox="0 0 180 255"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <rect fill="silver" x="0" y="0" width="180" height="255" />
-    <text
-      className="code tracked"
-      x="90"
-      y="100"
-      fontFamily="monospace"
-      fontSize="25"
-      fill="gray"
-      textAnchor="middle"
-    >
-      {title}
-    </text>
-    <text
-      className="code tracked"
-      x="90"
-      y="160"
-      fontFamily="monospace"
-      fontSize="18"
-      fill="gray"
-      textAnchor="middle"
-    >
-      {author}
-    </text>
+    <rect fill="silver" x="0" y="0" width="180" />
+    <foreignObject width={180} height={255} className="tc" y="75">
+      <p
+        xmlns="http://www.w3.org/1999/xhtml"
+        className="code tracked tc f3 gray"
+      >
+        {title}
+      </p>
+      <p
+        xmlns="http://www.w3.org/1999/xhtml"
+        className="code tracked tc f5 gray"
+      >
+        {author}
+      </p>
+    </foreignObject>
   </svg>
 )
 
