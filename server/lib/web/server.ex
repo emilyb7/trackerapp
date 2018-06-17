2238ea9c2f65395b9e12374e9ce0770e3d80917a
emily <limeyb7@gmail.com>
2018-06-17 18:57:38 +0100

    🚑  set server options correctly for prod env

--- server/lib/web/server.ex
+++ server/lib/web/server.ex
@@ -2,16 +2,31 @@ defmodule Server do
   use Application
   require Logger
 
-  def start(_type, _args) do
-    cowboy_options = [
+  def get_protocol(:prod), do: :http
+  def get_protocol(_env), do: :https
+
+  def get_options(:prod) do
+    [
+      otp_app: :tracker,
+      port: System.get_env("PORT") |> String.to_integer()
+    ]
+  end
+
+  def get_options(_env) do
+    [
       keyfile: "priv/keys/server.key",
       certfile: "priv/keys/server.crt",
       otp_app: :tracker,
       port: System.get_env("PORT") |> String.to_integer()
     ]
+  end
+
+  def start(_type, _args) do
+
+    env = Mix.env()
 
     children = [
-      Plug.Adapters.Cowboy.child_spec(scheme: :https, plug: Router, options: cowboy_options),
+      Plug.Adapters.Cowboy.child_spec(scheme: get_protocol(env), plug: Router, options: get_options(env)),
       Tracker.Repo
     ]
 
