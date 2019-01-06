1167f5bd817971f22b512a33c74fd6df7d4be6ee
emily <limeyb7@gmail.com>
2019-01-06 12:55:50 +0000

    🔒 fix security vulnerabilities, switch to from npm to yarn

--- client/src/Book/index.jsx
+++ client/src/Book/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link, } from 'react-router-dom'
 
 const Book = ({ book: { title, author, cover, isbn, ...rest }, }) => (
   <div className="pa2">
--- client/src/Result/Result.jsx
+++ client/src/Result/Result.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import { Link, } from 'react-router-dom'
+import { Link, withRouter, } from 'react-router-dom'
 
 import AddBook from '../AddBook/index.jsx'
 import Book from '../Book'
 
-const Result = ({ data, }) => (
-  <div className="bg-light-gray h-fill flex flex-column gray">
-    <Book data={data} />
-    <div className="mt2 pa2">
-      <AddBook data={data} />
-      <div className="ba bw1 b--silver br-pill ma2 pa3 ph4 tc">
-        <Link to="/add/scan" className="br-pill silver no-underline ">
-          Search again
-        </Link>
+const Result = ({ data, }) => {
+  const BookWithRouter = withRouter(Book)
+  return (
+    <div className="bg-light-gray h-fill flex flex-column gray">
+      <BookWithRouter data={data} />
+      <div className="mt2 pa2">
+        <AddBook data={data} />
+        <div className="ba bw1 b--silver br-pill ma2 pa3 ph4 tc">
+          <Link to="/add/scan" className="br-pill silver no-underline ">
+            Search again
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Result
